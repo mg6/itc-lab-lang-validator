6a1f5d5273e4a78a154cdfbefd788aaf4a61c6b5
Maciej Gamrat <maciej@gamrat.it>
2017-12-10 02:38:20 +0100

    Run all tests after fail and show run summary

--- test.sh
+++ test.sh
@@ -7,21 +7,28 @@ assert_diff() {
   local actual
 
   actual="$(${root_dir}/lab input || true)"
-  diff -wu expected /dev/stdin <<< "$actual" || { cat input; exit 1; }
+  diff -wu expected /dev/stdin <<< "$actual" || { cat input; return 1; }
 }
 
 run_suite() {
   local root_dir
+  local failed=0
+  local total=0
 
   root_dir="$(pwd)"
   shopt -s dotglob
 
   for path in tests/*; do
+    (( ++total ))
     echo "[test case] ${path}"
     pushd "$path" >/dev/null
-    assert_diff "$root_dir"
+    assert_diff "$root_dir" || (( ++failed ))
     popd >/dev/null
   done
+
+  local passed=$(( $total - $failed ))
+  echo "[summary] total ${total} / failed ${failed} / passed ${passed}"
+  [ $failed -eq 0 ]
 }
 
 run_suite
